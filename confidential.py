d45e649d5a8cf0095a84d128adc911545f4b0d6d
NavnitSinha <86523098+NavnitSinha@users.noreply.github.com>
2024-02-16 22:31:47 +0530

    confidential.py

---
+++ confidential.py
@@ -0,0 +1,4 @@
+# this folder is created to safeguard your details that are used to send emails. 
+your_email = "your_email_id@gmail.com"
+password = "your_password"
+# the password here has to be created from the google security settings
